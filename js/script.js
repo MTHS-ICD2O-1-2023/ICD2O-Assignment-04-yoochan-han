03fcedb8cb2c46f06178e4a80e8a57dd1c5d3521
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-30 21:45:53 +0000

    #

--- js/script.js
+++ js/script.js
@@ -4,5 +4,28 @@
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
 function Order() {
-  //pass
+  let totalCost = 0.00
+  // total cost is float number
+  let topping = []
+  const typeOfPizza = document.getElementById("typeOfPizza").value
+  const size = document.getElementById("size-1").checked
+  // get toppings information as array (T or F)
+  for(i=1;i<5;i++){
+    topping.push(document.getElementById("checkbox-" + String(i)).checked)   
+  }
+  // Check the type of pizza
+  if(typeOfPizza == cheese) {
+    
+  }
+  }
+
+function test() {
+  const test = document.getElementById("option-1").checked
+  document.getElementById("test").innerHTML = test
+  let topping = []
+  const size = document.getElementById("option-1").checked
+  for(i=1;i<5;i++){
+    topping.push(document.getElementById("checkbox-" + String(i)).checked)
+  }
+  document.getElementById("arr").innerHTML = topping
 }
