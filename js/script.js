745ad2ffc9b1a3c51da1860003ab070f612eb367
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-01 01:42:27 +0000

    #

--- js/script.js
+++ js/script.js
@@ -3,29 +3,48 @@
 // Created by: yoochan
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
-function Order() {
-  let totalCost = 0.00
+function orderThePizza() {
   // total cost is float number
+  let totalCost = 0.00
   let topping = []
-  const typeOfPizza = document.getElementById("typeOfPizza").value
+  let listOfOrder = []
+  let pizzatype
+  let pizzaSize
+  const cheesePizza = document.getElementById("pizzaType-1").checked
+  const baconPotatoPizza = document.getElementById("pizzaType-2").checked
   const size = document.getElementById("size-1").checked
   // get toppings information as array (T or F)
-  for(i=1;i<5;i++){
-    topping.push(document.getElementById("checkbox-" + String(i)).checked)   
-  }
-  // Check the type of pizza
-  if(typeOfPizza == cheese) {
-    
-  }
-  }
-
-function test() {
-  const test = document.getElementById("option-1").checked
-  document.getElementById("test").innerHTML = test
-  let topping = []
-  const size = document.getElementById("option-1").checked
-  for(i=1;i<5;i++){
+  for (i = 1; i < 5; i++) {
     topping.push(document.getElementById("checkbox-" + String(i)).checked)
   }
-  document.getElementById("arr").innerHTML = topping
+  //get pizza type 
+  //calculate the total cost
+  if (cheesePizza == true) {
+    pizzatype = "Cheese Pizza"
+    totalCost = 16.00
+  } else if (baconPotatoPizza == true) {
+    pizzatype = "Bacon Potato Pizza"
+    totalCost = 20.99
+  } else {
+    pizzatype = "Pineapple Pizza"
+    totalCost = 18.89
+  }
+  //get size of pizza
+  if (size == true) {
+    pizzaSize = "Large"
+  } else {
+    pizzaSize = "X-Large"
+    totalCost += 4.00
+  }
+  //get topping
+  for (l = 0; l < 4; l++) {
+    if (topping[l] == true) {
+      totalCost += 1.00 + 0.20 * l
+    }
+  }
+  totalCost = totalCost + totalCost*0.13
+  alert(totalCost)
+  //print total cost
+  document.getElementById("test").innerHTML = "The total cost of pizza is " + totalCost.toFixed(2) + "$"
 }
+
